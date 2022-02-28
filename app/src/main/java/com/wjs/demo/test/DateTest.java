f10bb29579a656fdb28e6228b7a57038efe5b307
万金胜 <wanjinshengwjs@163.com>
2022-02-28 23:52:19 +0800

    添加获取当前时间的完整日期格式的方法

---
+++ app/src/main/java/com/wjs/demo/test/DateTest.java
@@ -0,0 +1,10 @@
+package com.wjs.demo.test;
+
+import com.wjs.demo.utils.DateUtil;
+
+public class DateTest {
+
+    public static void getCurrentTimeFullDateFormatTest() {
+        DateUtil.getCurrentTimeFullDateFormat();
+    }
+}
